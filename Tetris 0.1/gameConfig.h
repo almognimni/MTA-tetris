4581b283a4a2db1b8622d9d88efedf6f60b3d354
NewStudentMS <123029828+NewStudentMS@users.noreply.github.com>
2024-01-12 16:15:25 +0200

    Update gameConfig.h
    בסיס

--- Tetris 0.1/gameConfig.h
+++ Tetris 0.1/gameConfig.h
@@ -4,14 +4,14 @@
 class GameConfig
 {
 public:
-	//enum class eKeys { LEFT = 'a', RIGHT = 's', UP = 'w', DOWN = 'z', ESC = 27 };
+	enum class eKeys { LEFT = 'a', RIGHT = 's', UP = 'w', DOWN = 'z', ESC = 27 };
 	static constexpr int GAME_WIDTH = 12;
 	static constexpr int GAME_HEIGHT = 18;
 
-	//static constexpr int MIN_X = 10;
-	//static constexpr int MIN_Y = 3;
+	static constexpr int MIN_X = 10;
+	static constexpr int MIN_Y = 3;
 
-	static const int COLORS[];
-	static const int NUM_OF_COLORS;
+	//static const int COLORS[];
+	//static const int NUM_OF_COLORS;
 };
 #endif
