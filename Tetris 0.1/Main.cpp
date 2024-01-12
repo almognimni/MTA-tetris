013c4f4f17be932fc97b0b61afffb9af9a5af8cc
NewStudentMS <123029828+NewStudentMS@users.noreply.github.com>
2024-01-12 16:17:43 +0200

    Update Main.cpp
    הדפס

--- Tetris 0.1/Main.cpp
+++ Tetris 0.1/Main.cpp
@@ -1,1 +1,15 @@
+#include <conio.h> // for kbhit+getch
 #include <iostream>
+#include <Windows.h> // for Sleep and colors
+using namespace std;
+
+#include "general.h"
+#include "gameConfig.h"
+//#include "point.h"
+#include "TetrisGame.h"
+
+void main()
+{
+	TetrisGame::drawBorder();
+	gotoxy(0, GameConfig::GAME_HEIGHT + GameConfig::MIN_Y + 2);
+}
